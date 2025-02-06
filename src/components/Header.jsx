cff8ee526c9acc41e9713db819a9805ae41ff865
Daramola Kehinde <daramolakenny18@gmail.com>
2025-02-06 14:12:39 +0100

    new commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+  import React from 'react'
+
+  const Header = () => {
+    return (
+      <div className='fixed top-0 left-0 z-10 w-full bg-slate-300'>
+        <div className='mx-auto py-4 '>Hello</div>
+      </div>
+    )
+  }
+
+  export default Header
