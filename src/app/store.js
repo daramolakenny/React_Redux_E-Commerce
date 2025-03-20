3c251de76786991de61e825829ed14889fb875a7
Daramola Kehinde <daramolakenny18@gmail.com>
2025-03-20 04:34:17 +0100

    new commit

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import sliderReducer from '../features/slicers/sliderslice';
 import productSlice from '../features/slicers/productslice';
-import cartSlice from '../features/slicers/cartSlice';
+import cartSlice from '../features/slicers/cartSlice.jsx';
 
 export const store = configureStore({
   reducer: {
     slider: sliderReducer,
     products: productSlice,
     cart: cartSlice,
-  },
+  }
 })
